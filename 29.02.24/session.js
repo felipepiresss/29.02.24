bd7f6a3a6957cb8c0e9bdf16d5463cce5c4afc67
Felipe <142436351+felipepiresss@users.noreply.github.com>
2024-02-29 12:25:57 -0300

    Add files via upload

---
+++ 29.02.24/session.js
@@ -0,0 +1,12 @@
+sessionStorage.clear;
+function enviarSessao() {
+    let txt_login = document.getElementById("txt_login");
+    let div_sessoes = document.getElementById("div_sessoes");
+
+    let sessao_digitada = sessionStorage.getItem(txt_login.value);
+    if (sessao_digitada == null) {
+        sessionStorage.setItem(txt_login.value, txt_login.value);
+        console.log(" A sessão " + txt_login.value + "foi logado com sucesso.");
+    } else {
+        console.error("Não foi possível enviar o Login" + txt_login.value + ", pois ja há uma sessão logada com esse login.");
+}
